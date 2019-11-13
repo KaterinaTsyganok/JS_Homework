28762cc8f0f1af0d431c813ebe847dc731aaaeb9
Katerina Tsyganok <katte002@gmail.com>
2019-11-13 19:43:20 +0300

    Fixed homework 11

--- Lesson-11/app.js
+++ Lesson-11/app.js
@@ -1,12 +1,9 @@
 //  Задание 1:
 
 function changeArr(arr) {
-
-    var newArr = arr.map(function(item) {
+    return arr.map(function(item) {
         return {'name': item};
     });
-
-    return newArr;
 }
 
 changeArr(['Peter', 'John', 'Kate']);
@@ -16,11 +13,9 @@ changeArr(['Peter', 'John', 'Kate']);
 //  Задание 2:
 
 function getTime(arr) {
-
     return arr.reduce(function(prev, curr) {
         return prev + ' : ' + curr;
     }, 'Текущее время');
-
 }
 
 getTime(['00', '13', '24']);
@@ -30,16 +25,13 @@ getTime(['00', '13', '24']);
 //  Задание 3:
 
 function getVowelNumber(text) {
-    
     var vowelArray = ['a', 'e', 'i', 'o', 'u'],
         vowelNumber = 0;
 
     var array = text.toLowerCase().split('');
 
     array.forEach(function(itemArray) {
-
         vowelArray.forEach(function(itemVowelArray) {
-
             if (itemArray === itemVowelArray) {
                 vowelNumber++;
             }
@@ -56,17 +48,14 @@ getVowelNumber('Hi! My name is Kate.');
 //  Задание 4:
 
 function changeText(text) {
-
     var textArr = text.split(/[.!?]/);
-
+    
     textArr.forEach(function(item, i) {
-
-        if (item.length < 1) {
+        if (!item.length) {
             textArr.splice(i, 1);
         } else {
             console.log(item.trim() + ' ' + item.split(/[,\s]/).join('').length);
-        }
-
+        }        
     });
 };
 
@@ -85,7 +74,6 @@ function getRepeatedWord(text) {
     var textArr = text.toLowerCase().split(/[.,!?\s]/).sort();
     
     for (var i = 0; i < textArr.length; i++) {
-
         while (textArr[i].length < 1) {
             textArr.splice(i, 1);
         }
@@ -101,8 +89,7 @@ function getRepeatedWord(text) {
             repeatedWordNumber = tempWordNumber;
         }
 
-        tempWordNumber = 1;
-        
+        tempWordNumber = 1;       
     };
 
     return 'Максимальное число повторений у слова "' + repeatedWord + '" - ' + repeatedWordNumber;
